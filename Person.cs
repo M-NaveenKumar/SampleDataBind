36c40edb3b02af20536c89ba162b793e6316362c
NaveenMadhamshetty <naveen080@ymail.com>
2019-10-14 22:58:47 +0530

    Initial Commit
    Uploading the initial commit
     1)Creates a view and Model.
     2)Initializes data on "Application Load" and binds the data context

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace SampleDataBind
+{
+
+    public class Person
+    {
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        private string _name;
+
+        private int _age;
+        public int Age { get { return _age; } set { _age = value; } }
+
+     
+    }
+        
+    public class vm
+    {
+        public  List<ObservableCollection<Person>> _person { get; set; }
+        public void LoadPersons ()
+        {
+            _person = new List<ObservableCollection<Person>>();
+            _person.Add(new ObservableCollection<Person>() { new Person() { Name = "Naveen", Age = 27 } });
+        }
+    }
+}
+
